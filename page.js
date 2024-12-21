6da72e4a17d13ea38ff0bb295326386506aacfef
achrafansari <achraf.ansari@centoria.fr>
2024-12-21 14:01:08 +0100

    add custom regex

--- page.js
+++ page.js
@@ -1,5 +1,7 @@
 
-const validation = new JustValidate('#form');
+const validation = new JustValidate('#form', {
+  validateBeforeSubmitting: true, 
+});
 
 validation
   .addField('#name', [
@@ -42,6 +44,36 @@ validation
       errorMessage: 'Common input est requis',
     },
   ])
+  .addField('#speciale', [
+    {
+      rule: 'required',
+      errorMessage: 'speciale est requis',
+    },
+    // {
+    //   rule: 'customRegexp',
+    //   value:`^\\d{0,8}$` ,
+    //   errorMessage: 'Le champ doit contenir uniquement des chiffres avec une longueur adaptée.',
+    // },
+    {
+      rule: 'custom',
+      validator: function(value) {
+        const input = document.querySelector('#speciale');
+        let regex = '^\\d{0,8}$'; 
+     
+        if (input.classList.contains('aa')) {
+          regex = '^\\d{0,8}$';  
+        } else if (input.classList.contains('bb')) {
+          regex = '^\\d{0,10}$'; 
+        } else if (input.classList.contains('cc')) {
+          regex = '^\\d{0,12}$'; 
+        }
+  
+        const regexTest = new RegExp(regex).test(value);
+        return regexTest;
+      },
+      errorMessage: 'Le champ doit contenir uniquement des chiffres avec une longueur adaptée.',
+    },
+  ])
   .onSuccess((event) => {
     event.preventDefault(); 
     console.log('Formulaire validé avec succès !');
@@ -71,4 +103,31 @@ document.getElementById('toggle-section-moral').addEventListener('change', funct
 document.getElementById('clear-errors').addEventListener('click', () => {
     validation.destroy(); 
     validation.refresh();
-  });
+ });
+////
+
+const specialeInput = document.getElementById('speciale');
+const selectOption = document.getElementById('selectOption');
+
+selectOption.addEventListener('change', function () {
+  // Supprimer toutes les classes précédentes
+  specialeInput.classList.remove('aa', 'bb', 'cc');
+
+  if (this.value === 'test1') {
+    specialeInput.classList.add('aa');
+    validation.revalidateField('#speciale');
+  } else if (this.value === 'test2') {
+    specialeInput.classList.add('bb');
+    validation.revalidateField('#speciale');
+  } else if (this.value === 'test3') {
+    specialeInput.classList.add('cc');
+    validation.revalidateField('#speciale');
+  }
+});
+
+specialeInput.addEventListener('input', () => {
+  validation.revalidateField('#speciale');
+});
+
+
+
